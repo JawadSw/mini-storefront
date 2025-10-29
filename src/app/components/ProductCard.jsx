710c9da1734561ebb978fa3d9d44fb029a5c3c07
Jawad Sweilem <jsweilem@usf.edu>
2025-10-29 16:45:39 -0400

    added some basic js on page.js to get some paceholder text to appear on the local host website.

---
+++ src/app/components/ProductCard.jsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function ProductCard() {
+  return (
+    <div>
+      <p>Product Card</p>
+    </div>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Mini Storefront </h1>
+      <p> products</p>
+    </main>
+  );
+}
